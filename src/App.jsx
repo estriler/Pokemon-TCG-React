8cda2ae9124c18be11c41336c7ac3f4b9e849eeb
Eric S <estriler@hotmail.com>
2024-08-26 23:00:41 -0500

    added attack function... working on hook syntax for activePokemonHP

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import './App.css'
 import base1 from "./components/base1.json"
 import HpBackgroundColor from './components/HpBackgroundColor';
+import Attack from './components/Attack';
 
 function App() {
+
+  // const [ activePokemonHP , setActivePokemonHP ] = useState({activeCard["hp"]});
+  // console.log({activeCard["hp"]});
+
+  const attackClick = () => {
+    setActivePokemonHP(Attack);
+  }
  
   let opponentActiveCard = base1[57];
   let pikachuCard = <img src={opponentActiveCard["images"]["small"]} />
 
   let activeCard = base1[59];
+  // firstActiveCardHP = activeCard["hp"];
   let ponytaCard = <img src={activeCard["images"]["small"]} />
+
+  // const [ activePokemonHP , setActivePokemonHP ] = useState({firstActiveCardHP});
 
   return (
     <div id='battleBoard'>
@@ -19,8 +30,10 @@ function App() {
       </div>
       <div id='activeCard'>
         {ponytaCard}
-        <button className='attackButton'>{activeCard["attacks"][0]["name"]}</button>
+        <button 
+          className='attackButton' onClick={attackClick}>{activeCard["attacks"][0]["name"]}</button>
         <div className='hp' style={{backgroundColor:HpBackgroundColor(activeCard)}}>{activeCard["hp"]} HP</div>
+        // {/* <div className='hp' style={{backgroundColor:HpBackgroundColor(activeCard)}}>{activePokemonHP} HP</div> */}
       </div>
     </div>
   )
---
+++ src/components/Attack.jsx
@@ -0,0 +1,3 @@
+export default function Attack(attackIndex){
+    return opponentHP = opponentHP - activeCard["attacks"][attackIndex]["damage"];
+}
