7ca6f33813b1862b673572e43b0fa2cdc9f019ab
Eric S <estriler@hotmail.com>
2024-08-27 20:52:25 -0500

    got opponent and active pkmn hp working

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,31 @@ import Attack from './components/Attack';
 
 function App() {
 
-  // const [ activePokemonHP , setActivePokemonHP ] = useState({activePokemonHP:'activeCard["hp"]'});
-  // const [ opponentPokemonHP , setOpponentPokemonHP ] = useState({hp:'opponentActiveCard["hp"]'});
-  // console.log({activeCard["hp"]});
-
-  const attackClick = (attackIndex) => {
-    // setActivePokemonHP(Attack);
-    // setOpponentPokemonHP(opponentPokemonHP - activeCard["attacks"][attackIndex]["damage"]);
-  }
-  
- 
   let opponentActiveCard = base1[57];
   let pikachuCard = <img src={opponentActiveCard["images"]["small"]} />
 
   let activeCard = base1[59];
-  // firstActiveCardHP = activeCard["hp"];
   let ponytaCard = <img src={activeCard["images"]["small"]} />
 
-  // const [ activePokemonHP , setActivePokemonHP ] = useState({firstActiveCardHP});
+  const [ activePokemonHP , setActivePokemonHP ] = useState(Number(activeCard["hp"]));
+  const [ opponentPokemonHP , setOpponentPokemonHP ] = useState(Number(opponentActiveCard["hp"]));
+
+  const attackClick = () => {
+    // setActivePokemonHP(Attack);
+    setOpponentPokemonHP(opponentPokemonHP - activeCard["attacks"][0]["damage"]);
+  }
 
   return (
     <div id='battleBoard'>
       <div id="opponentActiveCard">
         {pikachuCard}
-        <div className='hp' style={{backgroundColor:HpBackgroundColor(opponentActiveCard)}}>{opponentActiveCard["hp"]} HP</div>
+        {/* <div className='hp' style={{backgroundColor:HpBackgroundColor(opponentActiveCard)}}>{opponentActiveCard["hp"]} HP</div> */}
+        <div className='hp' style={{backgroundColor:HpBackgroundColor(opponentActiveCard)}}>{opponentPokemonHP} HP</div>
       </div>
       <div id='activeCard'>
         {ponytaCard}
-        <button 
-          className='attackButton' onClick={attackClick}>{activeCard["attacks"][0]["name"]}</button>
-        <div className='hp' style={{backgroundColor:HpBackgroundColor(activeCard)}}>{activeCard["hp"]} HP</div>
+        <button className='attackButton' onClick={attackClick}>{activeCard["attacks"][0]["name"]}</button>
+        <div className='hp' style={{backgroundColor:HpBackgroundColor(activeCard)}}>{activePokemonHP} HP</div>
         {/* <div className='hp' style={{backgroundColor:HpBackgroundColor(activeCard)}}>{activePokemonHP} HP</div> */}
       </div>
     </div>
